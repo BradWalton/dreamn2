c88e490271f5b5e05cbc6d6bdf5573e040c44ee6
BradWalton <walton4747@gmail.com>
2016-04-05 22:18:46 -0300

    adding compareTo

---
+++ assets/js/dependencies/compareTo.module.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Brad on 2016-04-05.
+ */
+angular.module('compareTo', []);
+angular.module('compareTo').directive('compareTo', function compareTo() {
+  return {
+    require: 'ngModel',
+    scope: {
+      otherModelValue: '=compareTo'
+    },
+    link: function(scope, element, attributes, ngModel) {
+
+      ngModel.$validators.compareTo = function(modelValue) {
+        return modelValue == scope.otherModelValue;
+      };
+
+      scope.$watch('otherModelValue', function() {
+        ngModel.$validate();
+      });
+    }
+  };
+});
